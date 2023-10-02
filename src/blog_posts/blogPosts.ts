6f61232c103067f4018d00d2f4a61507d7532da2
Ryan Keswick <ryank415@gmail.com>
2023-10-02 16:26:39 +1100

    renamed me-university => university slug

--- src/blog_posts/blogPosts.ts
+++ src/blog_posts/blogPosts.ts
@@ -10,6 +10,6 @@ export const blogPosts: BlogPost[] = [
     id: 0,
     title: "My University Journey",
     thumbnailImage: "graduation-caps-thrown-in-air.jpg",
-    slug: "me-university",
+    slug: "university",
   },
 ];
