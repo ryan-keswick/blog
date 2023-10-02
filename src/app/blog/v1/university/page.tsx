60c2972b724b6e727937c59a87328bb759ee5152
Ryan Keswick <ryank415@gmail.com>
2023-10-02 17:47:23 +1100

    working hackathon in website

--- src/app/blog/v1/university/page.tsx
+++ src/app/blog/v1/university/page.tsx
@@ -1,83 +1,66 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <main className="flex justify-center">
-      <div className="flex flex-col max-w-5xl">
-        <h1 className="">From Perth To UNSW</h1>
-        <br />
+    <div className="flex justify-center">
+      <main className="flex flex-col max-w-5xl justify-center">
+        <h1 className="self-center m-4 font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          From Perth To UNSW
+        </h1>
+        <div className="px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10">
+          <p>
+            I had the opportunity to participate in a hackathon when I was 15
+            years old. Our team won the hackathon by coding a{" "}
+            <p className="font-semibold inline-block">very</p> simple website
+            with plain HTML and CSS. This sparked an interest in coding that I
+            have not shaken since. Microsoft ran this event and forced us to use
+            GitHub and Azure so luckily the source code exists and I have got it
+            working on here, so take a peek into what a 15 year old could do
+            with no knowledge could do in 48 hours.
+            <div className="flex justify-center gap-5 my-5">
+              <Link
+                href={"/teamhydrosphere/index.html"}
+                target={"_blank"}
+                className="border-2 rounded-md p-2 hover:bg-white hover:text-black transition-all"
+              >
+                <h1>Winning Hackathon Website</h1>
+              </Link>
+              <a
+                href={"https://github.com/ryan-keswick/TeamHydroSphere"}
+                target={"_blank"}
+                className="border-2 rounded-md p-2 hover:bg-white hover:text-black transition-all"
+              >
+                <h1> Source Code (GitHub)</h1>
+              </a>
+            </div>
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          faucibus augue in metus convallis, at vestibulum erat bibendum.
-          Vestibulum eu mattis turpis. Cras convallis tristique dolor, in
-          lacinia orci. Donec fermentum, velit eget bibendum ullamcorper, sem
-          diam pharetra turpis, at convallis massa ligula iaculis nibh. Maecenas
-          cursus gravida lobortis. Fusce non est nec diam feugiat varius nec a
-          sapien. Fusce arcu sapien, ultrices eu nisl ac, tempor blandit velit.
-          Suspendisse aliquet tincidunt ultrices. Suspendisse potenti. Integer
-          dolor risus, gravida eu finibus et, congue id augue. Cras gravida
-          malesuada nunc eu rhoncus. Donec vitae convallis nisl. Donec vitae
-          fermentum purus, a hendrerit urna. Etiam fringilla gravida cursus.
-          Vestibulum sagittis egestas quam vitae convallis. Aliquam enim mi,
-          faucibus id lacus nec, ornare interdum sapien. Nam id quam ultrices,
-          venenatis libero non, posuere lacus. Suspendisse vel mattis velit, id
-          congue neque. Vestibulum cursus at est in eleifend. Pellentesque a
-          lacinia dolor, eu consectetur lectus. Ut in mollis purus.
-        </p>
-        <br />
+          <p>
+            It is pretty neat that HTML and CSS from 8 years still creates the
+            same webpage. All I did was put the GitHub project in the NextJS
+            public directory, perhaps simple is indeed better!
+          </p>
+          <br />
 
-        <p>
-          Praesent nisi ipsum, rutrum ac pharetra eu, aliquet eget ipsum.
-          Integer porttitor magna eu dui malesuada, a consequat velit dignissim.
-          Proin suscipit lectus at eleifend maximus. Suspendisse in lorem leo.
-          Donec quis dui magna. Maecenas elementum commodo nisl, nec lacinia
-          nulla. Cras ultricies nibh non diam fermentum, et cursus tellus
-          dictum. Sed gravida eros felis, eget dictum risus blandit eget. Nulla
-          hendrerit varius nunc, ut ultrices risus tincidunt dapibus.
-          Pellentesque dolor odio, consequat et ipsum a, volutpat fermentum
-          magna. Praesent rhoncus blandit bibendum. Sed congue velit eu nisi
-          mollis, eu aliquam sapien blandit. In ornare neque at tellus rutrum
-          laoreet. In lobortis bibendum leo, non malesuada arcu bibendum quis.
-          Nulla molestie orci odio, at iaculis nibh accumsan vitae. Nunc ut
-          malesuada justo. Donec eu orci elit. Sed quis auctor lorem. Proin ut
-          ullamcorper nibh, sit amet sagittis turpis. Nullam semper odio
-          scelerisque molestie molestie. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos.
-        </p>
-        <br />
+          <p></p>
+          <br />
 
-        <p>
-          Maecenas nunc sem, condimentum sed laoreet mollis, tincidunt non
-          sapien. Proin mollis turpis pharetra, ullamcorper ex sit amet, congue
-          dolor. Ut eu massa eu augue scelerisque consectetur id vel turpis.
-          Aliquam lectus leo, pulvinar eget feugiat in, maximus a enim. Nam
-          iaculis ex lorem, eget sodales nunc luctus eu. Maecenas imperdiet
-          finibus metus, sed ultrices nisl tristique vel. Morbi suscipit, sapien
-          eu porttitor tincidunt, odio augue efficitur tellus, quis varius nisl
-          augue at lacus. Donec magna leo, vehicula sit amet metus sed, interdum
-          bibendum nisl. In vitae ullamcorper urna, sed consequat eros. Nunc ac
-          turpis non libero condimentum rhoncus id eget metus. Nam lacus turpis,
-          aliquam eu massa vel, consectetur facilisis enim. Nulla mattis lacus
-          nunc, quis porta arcu vestibulum ac. Vestibulum ante ipsum primis in
-          faucibus orci luctus et ultrices posuere cubilia curae; Maecenas
-          pharetra feugiat mi quis lacinia. Nullam nec volutpat enim, in
-          accumsan velit. Sed rutrum sed enim vel interdum.
-        </p>
-        <br />
-
-        <p>
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas
-          tempor, ipsum et mattis iaculis, enim nulla iaculis nulla, sed
-          interdum odio leo id lectus. Suspendisse orci mi, tincidunt et
-          scelerisque in, luctus sit amet arcu. Proin diam arcu, iaculis in
-          turpis a, porta ultricies tortor. Donec lectus dui, posuere ut nibh
-          eu, gravida semper nisl. Suspendisse leo orci, feugiat et consectetur
-          nec, volutpat eget lorem. Sed at diam viverra, consequat felis vitae,
-          maximus nibh. Duis a nulla in est volutpat sollicitudin. Nullam
-          gravida tincidunt mi, a iaculis nisi gravida quis. Pellentesque
-          habitant morbi tristique senectus et netus et malesuada fames ac
-          turpis egestas. Nam nec fermentum urna, non volutpat nisl.
-        </p>
-      </div>
-    </main>
+          <p>
+            Interdum et malesuada fames ac ante ipsum primis in faucibus.
+            Maecenas tempor, ipsum et mattis iaculis, enim nulla iaculis nulla,
+            sed interdum odio leo id lectus. Suspendisse orci mi, tincidunt et
+            scelerisque in, luctus sit amet arcu. Proin diam arcu, iaculis in
+            turpis a, porta ultricies tortor. Donec lectus dui, posuere ut nibh
+            eu, gravida semper nisl. Suspendisse leo orci, feugiat et
+            consectetur nec, volutpat eget lorem. Sed at diam viverra, consequat
+            felis vitae, maximus nibh. Duis a nulla in est volutpat
+            sollicitudin. Nullam gravida tincidunt mi, a iaculis nisi gravida
+            quis. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Nam nec fermentum urna, non
+            volutpat nisl.
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }
