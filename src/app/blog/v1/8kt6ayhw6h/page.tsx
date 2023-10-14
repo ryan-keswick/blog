5d1cdbe281771004aa4c577dcdc8c78dc7ee9db9
Ryan Keswick <ryank415@gmail.com>
2023-10-14 18:50:34 +1100

    Finished first blog post

--- src/app/blog/v1/8kt6ayhw6h/page.tsx
+++ src/app/blog/v1/8kt6ayhw6h/page.tsx
@@ -10,10 +10,8 @@ export default function Page() {
         <header className="self-center m-4 font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
           <h1 className="text-3xl">Perth To UNSW Computer Science</h1>
         </header>
-        <div className="px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10">
-          <SubHeading subHeading="Introduction" />
-          <p>todo</p>
-          <SubHeading subHeading="Winning the Hackathon" />
+        <div className="flex flex-col gap-4 px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10">
+          <SubHeading subHeading="Winning a Hackathon" />
           <p>
             I had the opportunity to participate in a hackathon when I was 15.
             Each team had a topic to tackle and from the options provided we
@@ -23,7 +21,6 @@ export default function Page() {
             website in 48 hours, sparking an interest in coding that I have not
             shaken since.
           </p>
-          <br />
           <p>
             Microsoft ran this event and compelled us to use GitHub and Azure,
             so luckily, the source code exists, and I have got it working on
@@ -104,6 +101,63 @@ export default function Page() {
             {"Australia's "}
             tech capital or that UNSW is the best University producing Software
             Engineers in Australia. My choice of UNSW was just lucky.
+          </p>
+          <p>
+            Universities are run like businesses and will upsell you like a
+            business. I was upsold a whole other degree! It is honestly one of
+            the best businesses to run:
+          </p>
+          <ul className="mx-10 my-5 flex flex-col gap-2 ">
+            <li>1. Your customer is naive about the product. </li>
+            <li>
+              2. Your customer has tons of money, yet {"they're"} not worried
+              about spending it since {"it's"} the {"government's"} money.
+            </li>
+            <li>
+              3. Your customer is being pressured to purchase the product.
+              Pressured by their parents, friends, family and society in general
+            </li>
+            <li>
+              4. Your customer cannot easily switch to a competitor halfway
+              through their degree
+            </li>
+            <li>
+              5. Your customer will often finish their courses based on the sunk
+              cost fallacy.
+            </li>
+          </ul>
+          <p>
+            I enrolled at UNSW with a double degree in Mechatronic Engineering
+            and Computer Science. {'"'}Making robots is cool, and being better
+            at coding probably helps, right?{'"'} Mechatronic Engineering is a
+            combination of Mechanical, Electrical and Software engineering.
+            People spend many years studying these topics and are rewarded
+            handsomely for their work. A Mechatronic engineer spans all three
+            and ends up as a jack of all trades, master of none. Thus, they will
+            be paid as a mediocre engineer. There are few genuine mechatronic
+            jobs in Australia or even the world. Companies hire Software,
+            Hardware and Electrical teams and get them to work closely together.
+          </p>
+          <p>
+            At 18 years old, only a handful of people think about the cost of
+            the degree, let alone the opportunity cost (you could have been
+            earning money the entire time spent at University). I looked at the
+            price (~50K AUD for 5 years). It seemed like a lot of money, but
+            what other option did I have? Luckily, I liked coding, so why not
+            add 2 more years to make me more `employable` with mechatronics.
+          </p>
+          <p>
+            My conversations with friends and family sounded like, {'"'}I{"'"}m
+            moving to Sydney for Mechatronic Engineering and Computer Science
+            {'"'}. Everyone replied, {'"'}Wow, Ryan, you must be so smart{'"'}.
+            They were supportive and encouraging, which is nice. The society
+            holds Universities in such high esteem; why question that narrative?
+            Plus, most people don{"'"}t want confrontation, especially around
+            life decisions, so they act supportive.
+          </p>
+          <p>
+            My choice couldn{"'"}t have been better. I immensely enjoy working
+            as a Software Engineer.
           </p>
         </div>
       </div>
--- src/components/SubHeading/SubHeading.tsx
+++ src/components/SubHeading/SubHeading.tsx
@@ -3,7 +3,7 @@ interface SubHeadingProps {
 }
 
 const SubHeading = ({ subHeading }: SubHeadingProps) => {
-  return <h2 className="mt-5 mb-1 font-semibold text-2xl">{subHeading}</h2>;
+  return <h2 className="mb-[-16px] font-semibold text-2xl">{subHeading}</h2>;
 };
 
 export default SubHeading;
